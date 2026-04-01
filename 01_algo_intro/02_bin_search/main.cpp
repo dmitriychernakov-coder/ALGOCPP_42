34e30e528166793e9cc05b007c9d4db5b55997ea
Dmitriy Chernakov <dmitriychernakov@gmail.com>
2026-04-01 22:38:09 +0300

    initial init

---
+++ 01_algo_intro/02_bin_search/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+int countside(int arr[],int size,int point){
+    int left{0};
+    int right = size;
+    while (left < right){
+        int mid = left + ((right - left) / 2);
+        if (arr[mid] > point){
+            right = mid;
+        } else {
+            left = mid + 1;
+        }              
+    }
+    return size - left;
+
+}
+
+int  main(int argc, char** argv)
+{
+    int arr[] = {14, 16, 19, 32, 32, 32, 56, 69, 72};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int point;
+    std::cout << "Введите точку отсчета: " ;
+    std::cin >> point;
+    int result = countside(arr, size, point);
+    std::cout << "Количество элементов в массиве больших, чем:" << point <<  ": " << result << std::endl;
+    return 0;
+}
